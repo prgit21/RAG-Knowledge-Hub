f8ac23caf9bfc1b6a12e27ed64a9de5cc390a040
pr-extr21 <pbhat@extremenetworks.com>
2024-11-04 11:31:55 +0530

    SPA mfe with angular and react modules

---
+++ react-app/src/root.component.js
@@ -0,0 +1,10 @@
+export default function Root(props) {
+  return (
+    <section>
+      {props.name} is mounted!
+      <br />
+      <a href="/angular">Go To Angular App</a>
+      <a href="/dashboard">Go To Angular App</a>
+    </section>
+  );
+}
