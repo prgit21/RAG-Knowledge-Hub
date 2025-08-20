6824005d35a0dd6a6690a0121cb91c158827cccc
Pranav <96674501+prgit21@users.noreply.github.com>
2025-08-19 23:40:03 -0700

    fix: repair docker config

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:18
 WORKDIR /app
 COPY package*.json ./
-RUN npm install
+RUN npm ci
 COPY . .
 EXPOSE 9000
 CMD ["npm","start"]
--- start-servers.sh
+++ start-servers.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# Start Angular shell
-echo "Starting Angular shell ..."
-npm run start &  
+# Start root-config shell
+echo "Starting root-config ..."
+npm run start &
 
 # Starting Angular MFE's
-cd dashboard && npm run serve:single-spa:dashboard &  
-cd angular-app && npm run serve:single-spa:angular-app &
+(cd dashboard && npm run serve:single-spa:dashboard) &
+(cd angular-app && npm run serve:single-spa:angular-app) &
 
 # Starting React Parcel
-cd react-app && npm run start &  
-
-# # Start Node.js Backend
-# echo "Starting Node.js backend..."
-cd node-backend && nodemon server.js & 
+(cd react-app && npm run start) &
 
 # Start python backend
 echo "Starting FastAPI backend..."
-cd python-backend && uvicorn app.main:app --reload --port 3000 &
+(cd python-backend && uvicorn app.main:app --reload --port 8000) &
 
 wait
