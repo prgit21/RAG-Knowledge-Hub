c25cb08bd62e440306707f5a478631444618991f
Pranav <96674501+prgit21@users.noreply.github.com>
2025-08-20 00:10:19 -0700

    chore: remove svelte parcel

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.svelte$": "svelte-jester",
     "^.+\\.js$": "babel-jest",
   },
-  moduleFileExtensions: ["js", "svelte"],
-  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+  moduleFileExtensions: ["js"],
+  testMatch: ["<rootDir>/src/**/*.test.js"],
 };
